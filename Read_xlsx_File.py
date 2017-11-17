eb79d9899f6e2ab3a61c75d669b86bbe3f1f4fe9
Dylan Sabo <dylansaboo@gmail.com>
2017-11-17 17:27:00 -0500

    Update Read_xlsx_File.py

--- Read_xlsx_File.py
+++ Read_xlsx_File.py
@@ -2,6 +2,9 @@ import sys, pyexcel, os
 
 records = pyexcel.iget_records(file_name='SF_Contacts.xlsx')
 for record in records:
-    print("%s, %s, %s" % (record['First_Name'], record['Last_Name'], record['Email']))
-
-
+    print("Name: %s %s" % (record['First_Name'], record['Last_Name']))
+    print("      Email: %s" % (record['Email']))
+    print("      Phone: %s" % (record['Phone']))
+    print("      Title: %s" % (record['Title']))
+    print("      ")
+    print("")
